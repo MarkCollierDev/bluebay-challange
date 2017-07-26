e3e693f645250c240bfbfece2ce29ec9934f0df6
Unknown <mark.collier11@hotmail.com>
2017-07-26 12:39:58 +0100

    ready to submit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,142 @@
+<html>
+<head>
+    <title>Club dulux - Mexico</title>
+
+    <link rel="stylesheet" type="text/css" href="\node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="\node_modules/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="\css/style.css">
+</head>
+<body>
+
+<div class="col-md-10 col-md-offset-1">
+<div class="row" id="head-top">
+    <div class="col-md-3"><img src="\images/caribbean-warehouse-logo.png" style="height:40px;"></div>
+    <div class="col-md-1"></div>
+    <div class="col-md-2"></div>
+    
+    <div class="col-md-5 col-md-offset-1">
+        <div class="col-md-3 col-md-offset-2">
+        <a href="/#">My Booking</a>
+    </div>
+    <div class="col-md-6">
+        Follow Us
+        <i class="fa fa-twitter"></i>
+        <i class="fa fa-facebook"></i>
+        <i class="fa fa-instagram"></i>
+        <i class="fa fa-youtube-play"></i>
+    </div>
+    </div>
+</div>
+<div class="row" id="head-nav">
+    <div class="col-md-6">
+        <div class="col-md-2">Holidays</div>
+        <div class="col-md-2">Offers</div>
+        <div class="col-md-2">Cruise</div>
+        <div class="col-md-2">Weddings</div>
+        <div class="col-md-3">Win a holiday</div>
+        <div class="col-md-1">Search</div>
+    </div>  
+
+<div class="col-md-4 col-md-offset-2">
+    <i class="fa fa-envelope"></i> Get a Quote
+    <i class="fa fa-skype"></i>
+    <i class="fa fa-phone"></i> 08777 1234 123
+</div>
+
+</div>
+<div class="row" id="head-social">
+    <div class="col-md-6"><span class="fa-stack"> <i class="fa fa-circle fa-stack-2x yellow"></i> <i class="fa fa-bolt fa-stack-1x fa-small social-blue"></i> </span> MEAGA DEAL | MEXICO, 5<i class="fa fa-star fa-small"></i>  ALL INCLUSIVE FREE ROOM UPGRADE </div>
+<div class="col-md-3 col-md-offset-3">
+    <div class="col-md-8 col-md-offset-2">
+ <a href="/#"> SHARE</a>
+    <i class="fa fa-envelope"></i>
+    <i class="fa fa-twitter"></i>
+    <i class="fa fa-facebook"></i>
+</div>
+</div>
+</div>
+<div class="col-md-8" id="details">
+    <div class="image col-md-10 col-md-offset-1" id="main-image-div">
+        <img src="\images/hotel.jpeg" id="main-image">
+        <div class="col-md-12" id="main-image-caption">
+            <p class="bold black"> FREE Room Upgrade to Preferred Club Deluxe Ocean View + FREE 25 minutes Neck, Back and SHoulder Massage Spa treatment + FREE Gold Green Fees + FREE Unlimited-luxury Extras + Free Resort Transfers, Saving up to £3,438 per couple</p>
+        </div>
+    </div>
+    <div class="row"></div>
+    <div class="col-md-10 col-md-offset-1" id='description'>
+        <h2>The Deal . . .</h2>
+        <p>Exclusive FREE Room Upgrade to Junior Suite Swim-out or Junior Suite Oceanview saving up to £1,349 per couple</p>
+        <p class="bold">Amazing Value!</p>
+        <p>7 nights all inclusive from only £1,161pp <br> Exclusive Extras <br> <br> Exclusive FREE Room Upgrade to Junior Suite swim-Out or Junior Suite Oceanview</p>
+
+        <p class="bold">Boost Your Upgrade</p>
+        <ul>
+            <li> Boost your room upgrade to a roof top terrace Suite or OceansFront Excellence Club Suite worth up to £1,789 per couple from only £149pp per week </li>
+            <li>Double Boost your room upgrade to an Ocean Front Excellence Club roof top terrace Suite worth up to £2,009 per couple from only £249pp per week</li>
+            <br>
+
+        <li>Add shuttle transfers from £25pp</li>
+        <li>Add private transfers from £50pp</li>
+        <li>Add Luxury Private transfers from £75pp </li>
+        <li>Connect from your local Aairport</li>
+        </ul>
+        <p class="bold">Call now to customise this offer</p>
+    </div>
+</div>
+<div class="col-md-4" id="side-bar">
+    <div id="purple-offer" class="col-md-12">
+        <div id="offer-top">Available for 10 more days</div>
+        <div id="offer-lower">
+            <p>FROM </p>
+            <h1>£999</h1>
+            <div class="break-line"></div>
+            <p>Book by 31 July 2016</p>
+            <p class="small">price based on nothing</p>
+        </div>
+
+        <div class="row book">-------- how to book? --------</div>
+
+        <div class="col-md-12" id="call">Call Now On : 8520 741 934</div>
+        <div class="col-md-12" id="request">Request a Free Call</div>
+
+        <div class="col-md-12" id="why-we-like-it">
+            <h3>Why we like it</h3>
+
+            <p class="white">An Amazing 41% saving on incredible hotels in barbaydos located in the world famous St Lawrance Gap </p>
+        </div>
+
+        <div class="col-md-12" id="about-hotel">
+            <img id="about-img" src="\images\1.jpeg">
+            <div class="text">
+            <p class="bold">ABOUT THE HOTEL</p>
+            <P>If you have a taste for romance and relaxation, the all inclusive luxury of a random hotel is waiting to welcome you. Situated on a sugar white sand beach somewhere on the mexican coast.</P>
+            <p>this random hotel is a classic all-suite Mediterranean-style resort of stucco, tile and marble ...</p>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+    <div class="page-break col-md-12"><p class="bold">More Exclusive Offers</p></div>
+
+<div class="col-md-12" id="other-deals">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-3 deal">
+                <img src="\images/2.jpeg">
+                <p>£250pp</p>
+        </div>
+        <div class="col-md-3 deal">
+            <img src="\images/3.jpeg">
+            <p>£980pp</p>
+        </div>
+        <div class="col-md-3 deal">
+                <img src="\images/4.jpeg">
+                <p>£1850pp</p>
+        </div>
+    </div>
+</div>
+<div class="col-md-12" id="footer"></div>
+<div class="row"></div>
+</div>
+</body>
+</html>
